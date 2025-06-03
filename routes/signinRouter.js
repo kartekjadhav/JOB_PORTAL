9fd915f3214614f2b230b5fed048a628f16406f6
kartekjadhav <kartekjadhav2810@gmail.com>
2025-06-03 09:48:54 +0000

    Starting again

--- routes/signinRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { signinController } from "../controller/signinController.js"
-const signinRouter = express.Router()
-
-signinRouter.post("/signin", signinController)
-
-
-export default signinRouter
--- routes/signupRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { signupController } from "../controller/signupController.js";
-const signupRouter = express.Router();
-
-signupRouter.post("/signup", signupController)
-
-export default signupRouter
